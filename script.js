4f2feba4b60a833ee297a360ee65dfd37efebf79
Burl Adkins <adkins1393@gmail.com>
2019-05-23 13:49:12 -0400

    both forms work and structured 2nd form grid

--- script.js
+++ script.js
@@ -14,14 +14,13 @@ function renderResults(responseJson) {
     responseJson.forEach(brewery => {
         $('.js-results-list').append(
             `<li class="list-item">
-                <h3>${brewery.name}</h3>
-                <p>Brewery Type: ${brewery.brewery_type}</p>
+                <h4>${brewery.name}</h4>
                 <address>
                     <p>${brewery.street}</p>
-                    <p>${brewery.city}</p>
-                    <p>${brewery.state}</p>
-                    <p>${brewery.postal_code}</p>
+                    <p>${brewery.city}, ${brewery.state}, ${brewery.postal_code}</p>
                 </address>
+                <p>Brewery Type: ${brewery.brewery_type}</p>
+                <p>Phone: <a href="tel:${brewery.phone}">${brewery.phone}</a></p>
                 <p><a href="${brewery.website_url}">Click here to visit their website</a></p>
             </li>`
         );
@@ -43,7 +42,7 @@ function fetchResults(uri) {
 }
 
 /* ----- 2 ----- */
-function startFormParams(city, state, number, type) {
+function formParams(city, state, number, type) {
     const params = {
         by_city: city,
         by_state: state,
@@ -74,15 +73,30 @@ function getBrewery(paramsObj) {
 
 /* ----- 1 ----- */
 function handleStartForm() {
-    $('.js-start-form').submit(function(event) {
+    $('.js-start-form').on('submit', function(event) {
         event.preventDefault();
+        console.log(`handleStartForm is running`);
         const userBreweryCity = $('.js-brewery-city').val();
         const userBreweryState = $('.js-brewery-state').val();
         const userBreweryNumber = $('.js-brewery-number').val();
         const userBreweryType = $('.js-type-selector').val();
-        const paramsObj = startFormParams(userBreweryCity, userBreweryState, userBreweryNumber, userBreweryType);
+        const paramsObj = formParams(userBreweryCity, userBreweryState, userBreweryNumber, userBreweryType);
 
         $('.js-start-section').hide();
+        getBrewery(paramsObj);
+    });
+}
+
+function handleMainForm() {
+    $('.js-main-form').on('submit', function(event) {
+        event.preventDefault();
+        console.log(`handleMainForm is running`);
+        const userBreweryCity = $('.js-main-city').val();
+        const userBreweryState = $('.js-main-state').val();
+        const userBreweryNumber = $('.js-main-number').val();
+        const userBreweryType = $('.js-main-type').val();
+        const paramsObj = formParams(userBreweryCity, userBreweryState, userBreweryNumber, userBreweryType);
+
         getBrewery(paramsObj);
     });
 }
@@ -90,4 +104,5 @@ function handleStartForm() {
 $(function() {
     console.log('App ready');
     handleStartForm();
+    handleMainForm();
 });
